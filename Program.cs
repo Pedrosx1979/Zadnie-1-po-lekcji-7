d57441b44edf64f3d2d21ec3e2f85e2af7137665
Piotr Szydlak <pedrosx@o2.pl>
2023-12-02 09:11:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadnie_1_po_lekcji_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę:  ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj liczbę:  ");
+            int b = Int32.Parse(Console.ReadLine());
+
+            /*
+            int a = 5;
+            int b = 5;
+            Console.WriteLine("Liczba a = 5");
+            Console.ReadLine();
+            Console.WriteLine("Liczba b = 5");
+            Console.ReadLine();
+            if (a == b)
+            {
+                Console.WriteLine("Liczby są sobie równe");
+                Console.ReadLine();
+            }*/
+
+            if (a == b)
+            {
+                
+                Console.WriteLine("Liczby są sobie równe");
+                Console.ReadLine();
+            }
+            if (b != a) 
+            { 
+            
+                Console.WriteLine("Liczby nie są sobie równe");
+                Console.ReadLine();
+            }
+        }
+    }
+}
